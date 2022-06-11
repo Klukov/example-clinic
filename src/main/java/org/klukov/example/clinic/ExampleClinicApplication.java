cb457671ccb35300011cafe9887be7083635a9b9
Klukov <piotras.kluk@gmail.com>
2022-06-11 23:47:41 +0200

    init commit

---
+++ src/main/java/org/klukov/example/clinic/ExampleClinicApplication.java
@@ -0,0 +1,13 @@
+package org.klukov.example.clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleClinicApplication.class, args);
+    }
+
+}
