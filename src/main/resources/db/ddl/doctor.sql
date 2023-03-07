0f3ea022246a544f4a41195c3fb6f43fd8feed90
Klukov <piotras.kluk@gmail.com>
2023-03-07 20:30:15 +0100

    remove old sql scripts - replaced by flyway migrations

--- src/main/resources/db/ddl/doctor.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Doctor (
-    id bigserial NOT NULL,
-    first_name varchar(255) NULL,
-    last_name varchar(255) NULL,
-    rating numeric(19, 2) NULL,
-    specialization int4 NULL,
-    CONSTRAINT doctor_pkey PRIMARY KEY (id)
-);
--- src/main/resources/db/ddl/patient.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE public.patient (
-    id bigserial NOT NULL,
-    first_name varchar(255) NULL,
-    last_name varchar(255) NULL,
-    pesel_number varchar(255) NULL,
-    CONSTRAINT patient_pkey PRIMARY KEY (id)
-);
