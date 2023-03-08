5d5292cc60e894cd0dde457f417ae4a463740d1c
Klukov <piotras.kluk@gmail.com>
2023-03-08 01:26:49 +0100

    add pmd for static code analysis

--- src/main/java/org/klukov/example/clinic/domain/exception/ClinicRuntimeException.java
+++ src/main/java/org/klukov/example/clinic/domain/exception/ClinicRuntimeException.java
@@ -1,5 +1,6 @@
 package org.klukov.example.clinic.domain.exception;
 
+@SuppressWarnings({"PMD", "this object is not deserialized it does not need versionUUID"})
 public class ClinicRuntimeException extends RuntimeException {
 
     public ClinicRuntimeException(String message) {
