a04528f70071a75566dc7c8e00f598e7f94f1702
Klukov <piotras.kluk@gmail.com>
2023-03-11 17:54:52 +0100

    more verbose suppressWarning

--- src/main/java/org/klukov/example/clinic/domain/exception/ClinicRuntimeException.java
+++ src/main/java/org/klukov/example/clinic/domain/exception/ClinicRuntimeException.java
@@ -1,6 +1,6 @@
 package org.klukov.example.clinic.domain.exception;
 
-@SuppressWarnings({"PMD", "this object is not deserialized it does not need versionUUID"})
+@SuppressWarnings({"PMD.MissingSerialVersionUID"}) //this object is not deserialized it does not need versionUUID
 public class ClinicRuntimeException extends RuntimeException {
 
     public ClinicRuntimeException(String message) {
